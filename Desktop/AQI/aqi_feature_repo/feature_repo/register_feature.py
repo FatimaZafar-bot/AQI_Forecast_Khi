21e870d3a833e2493c74193b907700159ad9678b
FatimaZafarrr <fatima.zafarrr99@gmail.com>
2025-11-09 20:23:19 +0500

    Update register_feature.py

--- Desktop/AQI/aqi_feature_repo/feature_repo/register_feature.py
+++ Desktop/AQI/aqi_feature_repo/feature_repo/register_feature.py
@@ -7,8 +7,8 @@ from s3_utils import download_from_s3
 import sys
 import os
 # Add the parent folder (aqi_feature_repo/) to Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 PARQUET_PATH = "data/khi_air_quality_clean.parquet"
 
 # 🔹 Always download the latest processed data parquet from S3
