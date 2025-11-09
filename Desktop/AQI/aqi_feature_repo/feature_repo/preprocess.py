2c8706359997fa7531dd9a9a0d34cbdd248b8b9e
FatimaZafarrr <fatima.zafarrr99@gmail.com>
2025-11-09 20:03:59 +0500

    Update preprocess.py

--- Desktop/AQI/aqi_feature_repo/feature_repo/preprocess.py
+++ Desktop/AQI/aqi_feature_repo/feature_repo/preprocess.py
@@ -6,6 +6,11 @@ import pandas as pd
 import numpy as np
 import os
 from s3_utils import download_from_s3, upload_to_s3
+import sys
+
+# Add the parent folder (aqi_feature_repo/) to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 RAW_PATH = "karachi_air_quality.csv"
 PROCESSED_DIR = "data"
